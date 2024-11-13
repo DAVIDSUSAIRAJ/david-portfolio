cce9fc52eb7b6fd793e6b6acb4e291f84b96633e
Davidsusairaj s <davidsusairaj.sebasthiyar@solverminds.com>
2024-11-13 12:14:42 +0530

    Lazy load added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import Nav from "./component/nav";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Skills from "./pages/skills";
-import Publishes from "./pages/publishes";
-import Contact from "./pages/contact";
-// import Resume from "./pages/resume";
+
+// import Home from "./pages/home";
+// import About from "./pages/about";
+// import Skills from "./pages/skills";
+// import Publishes from "./pages/publishes";
+// import Contact from "./pages/contact";
+// // import Resume from "./pages/resume";
+
+// Lazy load page components
+const Home = lazy(() => import("./pages/home"));
+const About = lazy(() => import("./pages/about"));
+const Skills = lazy(() => import("./pages/skills"));
+const Publishes = lazy(() => import("./pages/publishes"));
+const Contact = lazy(() => import("./pages/contact"));
+// const Resume = lazy(() => import("./pages/resume"));
+
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* this is navbar component */}
         <Nav />
-        <Routes>
-          {/* this is pages */}
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/publishes" element={<Publishes />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* <Route path="/resume" element={<Resume />} /> */}
-          <Route path="/*" element={<Navigate to = "/" />} />
-          {/* <Route path="/:notf" element={<NotFound />} /> */}
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/publishes" element={<Publishes />} />
+            <Route path="/contact" element={<Contact />} />
+            {/* <Route path="/resume" element={<Resume />} /> */}
+            <Route path="/*" element={<Navigate to="/" />} />
+            {/* <Route path="/:notf" element={<NotFound />} /> */}
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
 }
-
 
 export default App;
